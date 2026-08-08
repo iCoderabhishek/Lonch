12ec9b4efba65e77dec9e3630636eab3a536e552
Abhishek Jha <iamabhishek1310@gmail.com>
2026-08-08 16:05:36 +0530

    implement server-sent events route and service for streaming deployment logs and update gitignore for logs directory '

---
+++ src/features/logs/services/subscriber.ts
@@ -0,0 +1,27 @@
+import type { Request, Response, NextFunction } from "express";
+import { redis } from "../../../shared/libs/redis";
+
+export const getLogs = async (req: Request, res: Response, next: NextFunction) => {
+    const deploymentId = req.params.deploymentId;
+
+    // sse headers 
+
+    res.setHeader("Content-Type", "text/event-stream");
+    res.setHeader("Cache-Control", "no-cache");
+    res.setHeader("Connection", "keep-alive");
+
+    const subscriber = redis.duplicate();
+    await subscriber.connect();
+
+    await subscriber.subscribe(`deploy-log:${deploymentId}`, (message: any) => {
+        res.write(`data: ${message}\n\n`);
+    });
+
+    req.on("close", async () => {
+        await subscriber.unsubscribe();
+        await subscriber.quit();
+    })
+
+}
+
+// after i built the frontend will use this built in api EventSource for listening.. like const eventSource = new EventSource(`/api/v1/logs/123/deploy-logs`);
